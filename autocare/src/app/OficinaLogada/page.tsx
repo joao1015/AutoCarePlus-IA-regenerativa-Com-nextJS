2c007fd89cf1d27cab75b8ceb48e6e4f000cce3e
ArthurBispo00 <arthurbispolima@hotmail.com>
2024-10-26 00:09:44 -0300

    Revisão no sidebar exclusivo de entrada

--- autocare/src/app/OficinaLogada/page.tsx
+++ autocare/src/app/OficinaLogada/page.tsx
@@ -1,10 +1,10 @@
-
 import Sidebar from "@/Components/Sideoficinas";
 import Image from "next/image";
 
 export default function Oficina() {
   return (
-  <Sidebar></Sidebar>
-   
+    <div className="w-[14%] h-screen bg-black p-10">
+      <Sidebar />
+    </div>
   );
 }
