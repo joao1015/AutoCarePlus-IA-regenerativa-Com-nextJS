43ee691934ecc8f587e2fa597c964c61ab98fce9
ArthurBispo00 <arthurbispolima@hotmail.com>
2024-10-26 00:28:08 -0300

    Atualizado mensagem na pagina de oficinas

--- autocare/src/app/OficinaLogada/page.tsx
+++ autocare/src/app/OficinaLogada/page.tsx
@@ -3,8 +3,29 @@ import Image from "next/image";
 
 export default function Oficina() {
   return (
-    <div className="w-[14%] h-screen bg-black p-10">
-      <Sidebar />
+    <div className="flex">
+      <div className="w-[14%] h-screen bg-black p-10">
+        <Sidebar />
+      </div>
+      <div className="flex-grow flex items-center justify-center bg-gray-100 h-screen">
+        <div className="text-center max-w-xl p-10">
+          <h1 className="text-4xl font-bold mb-4">
+            Gerencie sua Oficina de Forma Completa!
+          </h1>
+          <p className="text-lg mb-4">
+            Aqui você tem o controle total de sua oficina, podendo acompanhar todas as ordens de 
+            serviço em um único lugar. Visualize ordens recebidas, em andamento e finalizadas de 
+            maneira rápida e prática, e acesse detalhes importantes de cada uma delas.
+          </p>
+          <p className="text-lg mb-4">
+            Também é possível conferir as garantias associadas às ordens finalizadas, proporcionando 
+            um atendimento de pós-serviço de qualidade e a satisfação dos seus clientes.
+          </p>
+          <p className="text-lg font-bold text-blue-600">
+            Navegue pelo menu ao lado e descubra como simplificar o gerenciamento da sua oficina!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
