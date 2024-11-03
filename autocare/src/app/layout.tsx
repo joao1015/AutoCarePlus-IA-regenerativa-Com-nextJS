afba7aee4fa342008482f2e10c8d7e3be3c8aa82
joao1015 <moreira.joaopaulo1993@gmail.com>
2024-11-03 13:21:56 -0300

    corrigindo bug

--- autocare/src/app/layout.tsx
+++ autocare/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        {/* Adicionando a meta tag viewport para responsividade */}
+    
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
