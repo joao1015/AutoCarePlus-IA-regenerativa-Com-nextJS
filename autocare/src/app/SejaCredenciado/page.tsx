39cb683c326a45882ca85b759658bdc2db3c7021
joao1015 <moreira.joaopaulo1993@gmail.com>
2024-11-03 16:20:59 -0300

    teste

--- autocare/src/app/SejaCredenciado/page.tsx
+++ autocare/src/app/SejaCredenciado/page.tsx
@@ -1,16 +1,17 @@
+import Cabecalho from '@/Components/Cabecalho';
 import Formulario from '@/Components/FormularioCadastrocliente';
 import React from 'react';
 
 function Page() {
   return (
     <div>
-      
+      <div><Cabecalho></Cabecalho></div>
 
       <div className="flex items-center justify-center w-full" style={{ marginTop: '-7rem' }}>
         <Formulario />
       </div>
 
-      
+      <div><Cabecalho></Cabecalho></div>
     </div>
   );
 }
