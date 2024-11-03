35b526fc7d5c13e6fea5cbc8036ca94b83e16d92
joao1015 <moreira.joaopaulo1993@gmail.com>
2024-11-03 14:43:56 -0300

    teste

--- autocare/tailwind.config.js
+++ autocare/tailwind.config.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -6,13 +8,21 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   safelist: [
-    'bg-white', 'text-black', 'text-blue-500', 'border-blue-500', 
-    'hover:text-blue-500', 'hover:border-blue-500',
-    'md:flex', 'lg:flex', 'lg:gap-12', 'md:gap-8', 'lg:h-24', 'md:h-20',
-    'hidden', 'block', 'cursor-pointer', 'font-bold', 'transition-colors', 
-    'duration-300', 'rounded-lg', 'py-2', 'px-4',
+  
+      'bg-white', 'text-black', 'text-blue-500', 'border-blue-500', 
+      'hover:text-blue-500', 'hover:border-blue-500',
+      'md:flex', 'lg:flex', 'lg:gap-12', 'md:gap-8', 'lg:h-24', 'md:h-20',
+      'hidden', 'block', 'cursor-pointer', 'font-bold', 'transition-colors', 
+      'duration-300', 'rounded-lg', 'py-2', 'px-4',
+    
   ],
   plugins: [],
 };
