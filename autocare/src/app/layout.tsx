191adcbf4234ac9338508d5011bfe8cb8c067e31
joao1015 <moreira.joaopaulo1993@gmail.com>
2024-11-03 13:16:40 -0300

    config

--- autocare/src/app/layout.tsx
+++ autocare/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css"; 
+import './';
+
 import Cabecalho from "@/Components/Cabecalho"; // Importando o cabeçalho
 import Rodape from "@/Components/Rodape"; // Importando o rodapé
 
